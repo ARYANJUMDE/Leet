dd9f6ee309d891fa6f68eb67334f73fccf0cd322
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:58:16 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,8 @@
+# Last updated: 12/27/2025, 6:58:16 PM
+class Solution(object):
+    def multiply(self, num1, num2):
+        num1=int(num1)
+        num2=int(num2)
+        num1=num1*num2
+        return str(num1)
+        
