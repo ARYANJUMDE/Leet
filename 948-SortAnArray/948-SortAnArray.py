10657ea55de0b847c17ede68ccf641b261a43c2d
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:56:50 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.py
@@ -0,0 +1,14 @@
+# Last updated: 12/27/2025, 6:56:50 PM
+class Solution(object):
+    def sortArray(self, nums):
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if(nums[j]<nums[i]):
+        #             nums[i],nums[j]=nums[j],nums[i]
+        
+        
+        # return(nums)
+        nums.sort()
+        return nums
+        
+        
