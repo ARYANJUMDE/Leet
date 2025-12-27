2af43cf8593964cef660f90051a4fea010cc1b86
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:58:08 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,9 @@
+# Last updated: 12/27/2025, 6:58:08 PM
+class Solution(object):
+    def mySqrt(self, x):
+       import math
+       y=int(math.sqrt(x))
+       return y
+S=Solution()
+S.mySqrt(8) 
+        
