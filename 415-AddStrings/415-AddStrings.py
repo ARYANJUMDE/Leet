ba8e7124d550d40a6f850d964546ffe1c039ec1a
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:57:15 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.py
@@ -0,0 +1,11 @@
+# Last updated: 12/27/2025, 6:57:15 PM
+class Solution(object):
+    def addStrings(self, num1, num2):
+        num1=int(num1)
+        num2=int(num2)
+        sum=num1+num2
+        sum=str(sum)
+        return sum
+S=Solution()
+S.addStrings("11","123")
+        
