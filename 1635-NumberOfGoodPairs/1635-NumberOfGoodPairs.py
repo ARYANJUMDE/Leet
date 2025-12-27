1b3d3c88eeb7deb656ee6bd6155bb473e1cbd2a5
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:56:28 +0530

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.py
@@ -0,0 +1,11 @@
+# Last updated: 12/27/2025, 6:56:28 PM
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        count=0
+        for i in range(0,len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if(nums[i]==nums[j]):
+                    count=count+1
+                    
+        return(count)
+        
