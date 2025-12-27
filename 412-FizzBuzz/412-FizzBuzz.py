94639eafb26606b3be651a1199b0c9ef01ed5421
Aryan <jumdearyan7@gmail.com>
2025-12-27 18:57:18 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,16 @@
+# Last updated: 12/27/2025, 6:57:17 PM
+class Solution(object):
+    def fizzBuzz(self, n):
+        answer=[]
+        for i in range(1,n+1):
+            if(i%3==0 and i%5==0):
+                answer.append("FizzBuzz")
+            elif(i%3==0):
+                answer.append("Fizz")
+            elif(i%5==0):
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return (answer)
+
+        
