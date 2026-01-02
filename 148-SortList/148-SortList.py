08899d91c71768e465c98866e78bbfd0ac92bbb1
Aryan <jumdearyan7@gmail.com>
2026-01-02 15:23:15 +0530

    File updated at 1/2/2026, 3:23:15 PM

--- 148-SortList/148-SortList.py
+++ 148-SortList/148-SortList.py
@@ -1,52 +1,52 @@
-# Last updated: 12/27/2025, 6:57:59 PM
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# class Solution(object):
-#     def sortList(self,head):
-#         current=head
-#         while(current!=None):
-#             next_node=current.next
-#             while(next_node!=None):
-#                 if(current.val>next_node.val):
-#                     current.val,next_node.val=next_node.val,current.val
-#                 next_node=next_node.next
-#             current=current.next
-#         return head
-        
-class Solution(object):
-    def sortList(self, head):
-        # Base case: if the list is empty or has only one node
-        if not head or not head.next:
-            return head
-
-        # Step 1: Split list into two halves
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        mid = slow.next
-        slow.next = None  # Break the list into two halves
-
-        # Step 2: Sort both halves recursively
-        left = self.sortList(head)
-        right = self.sortList(mid)
-
-        # Step 3: Merge sorted halves
-        return self.merge(left, right)
-
-    def merge(self, l1, l2):
-        dummy = tail = ListNode(0)
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next, l1 = l1, l1.next
-            else:
-                tail.next, l2 = l2, l2.next
-            tail = tail.next
-        tail.next = l1 or l2
-        return dummy.next
-
-        
+# Last updated: 1/2/2026, 3:23:15 PM
+1# Definition for singly-linked list.
+2# class ListNode(object):
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6# class Solution(object):
+7#     def sortList(self,head):
+8#         current=head
+9#         while(current!=None):
+10#             next_node=current.next
+11#             while(next_node!=None):
+12#                 if(current.val>next_node.val):
+13#                     current.val,next_node.val=next_node.val,current.val
+14#                 next_node=next_node.next
+15#             current=current.next
+16#         return head
+17        
+18class Solution(object):
+19    def sortList(self, head):
+20        # Base case: if the list is empty or has only one node
+21        if not head or not head.next:
+22            return head
+23
+24        # Step 1: Split list into two halves
+25        slow, fast = head, head.next
+26        while fast and fast.next:
+27            slow = slow.next
+28            fast = fast.next.next
+29
+30        mid = slow.next
+31        slow.next = None  # Break the list into two halves
+32
+33        # Step 2: Sort both halves recursively
+34        left = self.sortList(head)
+35        right = self.sortList(mid)
+36
+37        # Step 3: Merge sorted halves
+38        return self.merge(left, right)
+39
+40    def merge(self, l1, l2):
+41        dummy = tail = ListNode(0)
+42        while l1 and l2:
+43            if l1.val < l2.val:
+44                tail.next, l1 = l1, l1.next
+45            else:
+46                tail.next, l2 = l2, l2.next
+47            tail = tail.next
+48        tail.next = l1 or l2
+49        return dummy.next
+50
+51        
