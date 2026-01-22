261e64a5aa1a011a48eb939f6b1c13a1534b2e89
Aryan <jumdearyan7@gmail.com>
2026-01-22 11:43:10 +0530

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ 1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.py
@@ -0,0 +1,11 @@
+# Last updated: 1/22/2026, 11:43:10 AM
+class Solution(object):
+    def countNegatives(self, grid):
+        count=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]<0:
+                    count=count+1
+        
+        return(count)
+        
