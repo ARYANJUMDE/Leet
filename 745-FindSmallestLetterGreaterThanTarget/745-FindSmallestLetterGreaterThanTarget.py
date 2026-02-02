2b1b84a03c5c8ad68b8d8919e89583a43ebcbb7f
Aryan <jumdearyan7@gmail.com>
2026-02-02 21:25:54 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,15 @@
+# Last updated: 2/2/2026, 9:25:53 PM
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        x=[]
+        for i in range(len(letters)):
+            x.append(letters[i])
+        heapq.heapify(letters)
+        while len(letters)>0:
+            if letters[0]>target:
+                return(letters[0])
+
+            else:
+                heapq.heappop(letters)
+        return x[0]
+        
