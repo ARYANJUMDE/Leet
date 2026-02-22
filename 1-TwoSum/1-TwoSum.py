f42edf009909b7e10cc09c1d022d4e8c09cca833
Aryan <jumdearyan7@gmail.com>
2026-02-22 15:20:19 +0530

    File updated at 2/22/2026, 3:20:20 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,10 +1,10 @@
-# Last updated: 12/27/2025, 6:58:34 PM
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(0,len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if(nums[i]+nums[j]==target):
-                    return [i,j]
-S=Solution()
-S.twoSum([2,7,11,15],9)
-        
+# Last updated: 2/22/2026, 3:20:20 PM
+1class Solution(object):
+2    def twoSum(self, nums, target):
+3        for i in range(0,len(nums)-1):
+4            for j in range(i+1,len(nums)):
+5                if(nums[i]+nums[j]==target):
+6                    return [i,j]
+7S=Solution()
+8S.twoSum([2,7,11,15],9)
+9        
