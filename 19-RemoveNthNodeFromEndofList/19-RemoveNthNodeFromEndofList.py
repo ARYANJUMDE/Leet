72841e4707ed9914c3e33d79e83afe4fc31f7e14
Aryan <jumdearyan7@gmail.com>
2026-03-16 20:12:12 +0530

    File updated at 3/16/2026, 8:12:10 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,34 +1,22 @@
-# Last updated: 3/16/2026, 6:57:45 PM
-1class MinStack(object):
-2
-3    def __init__(self):
-4        self.stack=[]
-5        self.minStack=[]
-6
-7    def push(self, val):
-8        self.stack.append(val)
-9        if not self.minStack or val<=self.minStack[-1]:
-10            self.minStack.append(val)
-11        
-12    def pop(self):
-13
-14        if self.minStack[-1]==self.stack[-1]:
-15            self.minStack.pop()
-16        self.stack.pop()
-17        
-18
-19    def top(self):
-20        return self.stack[-1]
-21        
-22
-23    def getMin(self):
-24        return self.minStack[-1]
-25        
-26
-27
-28# Your MinStack object will be instantiated and called as such:
-29# obj = MinStack()
-30# obj.push(val)
-31# obj.pop()
-32# param_3 = obj.top()
-33# param_4 = obj.getMin()
+# Last updated: 3/16/2026, 8:12:10 PM
+1class Solution(object):
+2    def evalRPN(self, tokens):
+3        stack = []
+4
+5        for t in tokens:
+6            if t in "+-*/":
+7                b = stack.pop()
+8                a = stack.pop()
+9
+10                if t == "+":
+11                    stack.append(a + b)
+12                elif t == "-":
+13                    stack.append(a - b)
+14                elif t == "*":
+15                    stack.append(a * b)
+16                else:   # division
+17                    stack.append(int(float(a) / b))   # truncate toward zero
+18            else:
+19                stack.append(int(t))
+20
+21        return stack.pop()
