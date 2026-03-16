1c672806fef054efd091d7fe8f3e50f1ae4a4446
Aryan <jumdearyan7@gmail.com>
2026-03-16 18:48:38 +0530

    File updated at 3/16/2026, 6:48:36 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,20 +1,28 @@
-# Last updated: 3/16/2026, 6:29:04 PM
-1class Solution(object):
-2    def isValid(self, s):
-3        stack=[]
-4        for ch in s:
-5            if(ch in "({["):
-6                stack.append(ch)
-7            else:
-8                if len(stack)==0:
-9                    return False
-10                top=stack.pop()
-11
-12                if ((ch==")" and top!="(") or (ch=="]" and top!="[") or (ch=="}" and top!="{")):
+# Last updated: 3/16/2026, 6:48:36 PM
+1from  collections import deque
+2class MyStack(object):
+3
+4    def __init__(self):
+5        self.q1=deque()
+6        self.q2=deque()
+7
+8    def push(self, x):
+9        self.q2.append(x)
+10        while self.q1:
+11            self.q2.append(self.q1.popleft())
+12        self.q1,self.q2=self.q2,self.q1
 13
-14
-15                    return False
-16        return len(stack)==0
+14    def pop(self):
+15        return self.q1.popleft()
+16    def top(self):
 17
-18        
-19        
+18        return self.q1[0]
+19    def empty(self):
+20        return len(self.q1)==0
+21        
+22# Your MyStack object will be instantiated and called as such:
+23# obj = MyStack()
+24# obj.push(x)
+25# param_2 = obj.pop()
+26# param_3 = obj.top()
+27# param_4 = obj.empty()
