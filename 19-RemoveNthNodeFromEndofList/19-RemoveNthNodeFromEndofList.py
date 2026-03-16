bf0641ab1bb5c1793196c79089bfa8a8f6068b34
Aryan <jumdearyan7@gmail.com>
2026-03-16 20:17:32 +0530

    File updated at 3/16/2026, 8:17:31 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,52 +1,75 @@
-# Last updated: 3/16/2026, 8:15:54 PM
-1class MyCircularQueue(object):
+# Last updated: 3/16/2026, 8:17:31 PM
+1class MyCircularDeque(object):
 2
 3    def __init__(self, k):
-4        self.q = [0] * k
-5        self.k = k
+4        self.k = k
+5        self.q = [0] * k
 6        self.front = 0
 7        self.rear = -1
 8        self.count = 0
 9
-10    def enQueue(self, value):
+10    def insertFront(self, value):
 11        if self.isFull():
 12            return False
 13        
-14        self.rear = (self.rear + 1) % self.k
-15        self.q[self.rear] = value
+14        self.front = (self.front - 1) % self.k
+15        self.q[self.front] = value
 16        self.count += 1
-17        return True
-18
-19    def deQueue(self):
-20        if self.isEmpty():
-21            return False
-22        
-23        self.front = (self.front + 1) % self.k
-24        self.count -= 1
-25        return True
-26
-27    def Front(self):
-28        if self.isEmpty():
-29            return -1
-30        return self.q[self.front]
+17        
+18        if self.count == 1:
+19            self.rear = self.front
+20            
+21        return True
+22
+23    def insertLast(self, value):
+24        if self.isFull():
+25            return False
+26        
+27        self.rear = (self.rear + 1) % self.k
+28        self.q[self.rear] = value
+29        self.count += 1
+30        return True
 31
-32    def Rear(self):
+32    def deleteFront(self):
 33        if self.isEmpty():
-34            return -1
-35        return self.q[self.rear]
-36
-37    def isEmpty(self):
-38        return self.count == 0
+34            return False
+35        
+36        self.front = (self.front + 1) % self.k
+37        self.count -= 1
+38        return True
 39
-40    def isFull(self):
-41        return self.count == self.k        
-42
-43
-44# Your MyCircularQueue object will be instantiated and called as such:
-45# obj = MyCircularQueue(k)
-46# param_1 = obj.enQueue(value)
-47# param_2 = obj.deQueue()
-48# param_3 = obj.Front()
-49# param_4 = obj.Rear()
-50# param_5 = obj.isEmpty()
-51# param_6 = obj.isFull()
+40    def deleteLast(self):
+41        if self.isEmpty():
+42            return False
+43        
+44        self.rear = (self.rear - 1) % self.k
+45        self.count -= 1
+46        return True
+47
+48    def getFront(self):
+49        if self.isEmpty():
+50            return -1
+51        return self.q[self.front]
+52
+53    def getRear(self):
+54        if self.isEmpty():
+55            return -1
+56        return self.q[self.rear]
+57
+58    def isEmpty(self):
+59        return self.count == 0
+60
+61    def isFull(self):
+62        return self.count == self.k
+63
+64
+65# Your MyCircularDeque object will be instantiated and called as such:
+66# obj = MyCircularDeque(k)
+67# param_1 = obj.insertFront(value)
+68# param_2 = obj.insertLast(value)
+69# param_3 = obj.deleteFront()
+70# param_4 = obj.deleteLast()
+71# param_5 = obj.getFront()
+72# param_6 = obj.getRear()
+73# param_7 = obj.isEmpty()
+74# param_8 = obj.isFull()
