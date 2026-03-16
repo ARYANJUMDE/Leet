2f7f3831ce5955ae2052805dba7023496103ab81
Aryan <jumdearyan7@gmail.com>
2026-03-16 20:47:38 +0530

    File updated at 3/16/2026, 8:47:37 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,44 +1,16 @@
-# Last updated: 3/16/2026, 8:31:21 PM
-1# Definition for singly-linked list.
-2# class ListNode(object):
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution(object):
-7    def reorderList(self, head):
-8        if not head or not head.next:
-9            return
-10        
-11        # 1. Find middle
-12        slow = head
-13        fast = head
-14        
-15        while fast and fast.next:
-16            slow = slow.next
-17            fast = fast.next.next
-18        
-19        # 2. Reverse second half
-20        prev = None
-21        curr = slow.next
-22        slow.next = None
-23        
-24        while curr:
-25            temp = curr.next
-26            curr.next = prev
-27            prev = curr
-28            curr = temp
-29        
-30        # 3. Merge two lists
-31        first = head
-32        second = prev
-33        
-34        while second:
-35            temp1 = first.next
-36            temp2 = second.next
-37            
-38            first.next = second
-39            second.next = temp1
-40            
-41            first = temp1
-42            second = temp2
-43        
+# Last updated: 3/16/2026, 8:47:37 PM
+1class Solution(object):
+2    def dailyTemperatures(self, temperatures):
+3        n = len(temperatures)
+4        answer = [0]*n
+5        stack = []
+6
+7        for i in range(n):
+8            
+9            while stack and temperatures[i] > temperatures[stack[-1]]:
+10                prev = stack.pop()
+11                answer[prev] = i - prev
+12            
+13            stack.append(i)
+14
+15        return answer
