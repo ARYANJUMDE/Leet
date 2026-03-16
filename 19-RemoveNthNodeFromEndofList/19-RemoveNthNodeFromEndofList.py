f064e253bc14fdfdb0a3a905669bb16c76facc6f
Aryan <jumdearyan7@gmail.com>
2026-03-16 20:28:45 +0530

    File updated at 3/16/2026, 8:28:44 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,29 +1,27 @@
-# Last updated: 3/16/2026, 8:26:57 PM
+# Last updated: 3/16/2026, 8:28:44 PM
 1# Definition for singly-linked list.
 2# class ListNode(object):
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6class Solution(object):
-7    def partition(self, head, x):
-8        beforeHead = ListNode(0)
-9        afterHead = ListNode(0)
-10        
-11        before = beforeHead
-12        after = afterHead
-13        
-14        while head:
-15            if head.val < x:
-16                before.next = head
-17                before = before.next
-18            else:
-19                after.next = head
-20                after = after.next
-21            
-22            head = head.next
-23        
-24        after.next = None
-25        before.next = afterHead.next
-26        
-27        return beforeHead.next
-28        
+7    def reverseBetween(self, head, left, right):
+8        
+9        dummy = ListNode(0)
+10        dummy.next = head
+11        prev = dummy
+12        
+13        # move prev to node before 'left'
+14        for i in range(left-1):
+15            prev = prev.next
+16        
+17        curr = prev.next
+18        
+19        # reverse the part
+20        for i in range(right-left):
+21            temp = curr.next
+22            curr.next = temp.next
+23            temp.next = prev.next
+24            prev.next = temp
+25        
+26        return dummy.next
