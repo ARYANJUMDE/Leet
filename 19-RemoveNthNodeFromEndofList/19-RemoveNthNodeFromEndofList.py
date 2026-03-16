6d0c075cd4bf9a0d8c2240e23b226174edc6e03a
Aryan <jumdearyan7@gmail.com>
2026-03-16 20:29:59 +0530

    File updated at 3/16/2026, 8:29:57 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,27 +1,39 @@
-# Last updated: 3/16/2026, 8:28:44 PM
-1# Definition for singly-linked list.
-2# class ListNode(object):
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution(object):
-7    def reverseBetween(self, head, left, right):
-8        
-9        dummy = ListNode(0)
-10        dummy.next = head
-11        prev = dummy
+# Last updated: 3/16/2026, 8:29:57 PM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, x, next=None, random=None):
+5        self.val = int(x)
+6        self.next = next
+7        self.random = random
+8"""
+9
+10class Solution(object):
+11    def copyRandomList(self, head):
 12        
-13        # move prev to node before 'left'
-14        for i in range(left-1):
-15            prev = prev.next
-16        
-17        curr = prev.next
-18        
-19        # reverse the part
-20        for i in range(right-left):
-21            temp = curr.next
-22            curr.next = temp.next
-23            temp.next = prev.next
-24            prev.next = temp
+13        if not head:
+14            return None
+15        
+16        oldToNew = {}
+17        
+18        curr = head
+19        
+20        # create copy of each node
+21        while curr:
+22            copy = Node(curr.val)
+23            oldToNew[curr] = copy
+24            curr = curr.next
 25        
-26        return dummy.next
+26        curr = head
+27        
+28        # assign next and random pointers
+29        while curr:
+30            copy = oldToNew[curr]
+31            
+32            copy.next = oldToNew.get(curr.next)
+33            copy.random = oldToNew.get(curr.random)
+34            
+35            curr = curr.next
+36        
+37        return oldToNew[head]
+38        
