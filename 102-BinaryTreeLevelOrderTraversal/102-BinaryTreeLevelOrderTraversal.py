495131bb036aa3c753d7cbfc17bf0e6a6acb6164
Aryan <jumdearyan7@gmail.com>
2026-03-26 20:32:08 +0530

    File updated at 3/26/2026, 8:32:08 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,33 +1,28 @@
-# Last updated: 3/26/2026, 8:26:08 PM
+# Last updated: 3/26/2026, 8:32:08 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, val=0, left=None, right=None):
 4#         self.val = val
 5#         self.left = left
 6#         self.right = right
-7from collections import deque
-8
-9class Solution(object):
-10    def rightSideView(self, root):
-11        if root is None:
-12            return []
-13        
-14        result = []
-15        q = deque([root])
-16        
-17        while q:
-18            size = len(q)
-19            
-20            for i in range(size):
-21                node = q.popleft()
-22                
-23                if i == size - 1:
-24                    result.append(node.val)
-25                
-26                if node.left:
-27                    q.append(node.left)
-28                if node.right:
-29                    q.append(node.right)
-30        
-31        return result
-32        
+7class Solution(object):
+8    def isBalanced(self, root):
+9        
+10        def check(node):
+11            if node is None:
+12                return 0  
+13            
+14            left = check(node.left)
+15            if left == -1:
+16                return -1
+17            
+18            right = check(node.right)
+19            if right == -1:
+20                return -1
+21            
+22            if abs(left - right) > 1:
+23                return -1
+24            
+25            return 1 + max(left, right)
+26        
+27        return check(root) != -1        
