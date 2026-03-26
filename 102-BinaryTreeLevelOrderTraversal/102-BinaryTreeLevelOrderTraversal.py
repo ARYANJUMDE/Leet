7101d052728c920f6bc40beba195a82fd13278f7
Aryan <jumdearyan7@gmail.com>
2026-03-26 20:53:39 +0530

    File updated at 3/26/2026, 8:53:38 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 3/26/2026, 8:46:46 PM
+# Last updated: 3/26/2026, 8:53:38 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,28 +6,16 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution(object):
-8    def deleteNode(self, root, key):
-9        if root is None:
-10            return None
-11        
-12        if key < root.val:
-13            root.left = self.deleteNode(root.left, key)
-14        
-15        elif key > root.val:
-16            root.right = self.deleteNode(root.right, key)
-17        
-18        else:
-19            if root.left is None:
-20                return root.right
-21            if root.right is None:
-22                return root.left
-23            
-24            temp = root.right
-25            while temp.left:
-26                temp = temp.left
-27            
-28            root.val = temp.val
-29            
-30            root.right = self.deleteNode(root.right, temp.val)
-31        
-32        return root        
+8    def maxPathSum(self, root):
+9        self.max_sum=float('-inf')
+10        def dfs(node):
+11            if not node:
+12                return 0
+13            left=max(dfs(node.left),0)
+14            right=max(dfs(node.right),0)
+15            self.max_sum=max(self.max_sum,left+right+node.val)
+16            return node.val+max(left,right)
+17
+18        dfs(root)
+19        return self.max_sum
+20        
