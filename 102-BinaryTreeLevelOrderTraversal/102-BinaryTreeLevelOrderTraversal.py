81ba1f0e9e1eda728f02c316d5e48b683897a77f
Aryan <jumdearyan7@gmail.com>
2026-03-26 20:38:52 +0530

    File updated at 3/26/2026, 8:38:52 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 3/26/2026, 8:37:00 PM
+# Last updated: 3/26/2026, 8:38:52 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,32 +6,16 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution(object):
-8    def buildTree(self, preorder, inorder):
-9        if not preorder or not inorder:
-10            return None
-11        
-12        inorder_map = {val: i for i, val in enumerate(inorder)}
-13        
-14        self.pre_idx = 0
-15        
-16        def helper(left, right):
-17            if left > right:
-18                return None
-19            
-20            
-21            root_val = preorder[self.pre_idx]
-22            self.pre_idx += 1
-23            
-24            root = TreeNode(root_val)
-25            
-26            
-27            index = inorder_map[root_val]
-28            
-29            
-30            root.left = helper(left, index - 1)
-31            root.right = helper(index + 1, right)
-32            
-33            return root
-34        
-35        return helper(0, len(inorder) - 1)
-36        
+8    def isValidBST(self, root):
+9        
+10        def check(node, low, high):
+11            if node is None:
+12                return True
+13            
+14            if not (low < node.val < high):
+15                return False
+16            
+17            return (check(node.left, low, node.val) and
+18                    check(node.right, node.val, high))
+19        
+20        return check(root, float('-inf'), float('inf'))        
