d9db569f2c6a25da20682b0ea7a67fb1f696d48e
Aryan <jumdearyan7@gmail.com>
2026-03-26 20:46:47 +0530

    File updated at 3/26/2026, 8:46:46 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,19 +1,33 @@
-# Last updated: 3/26/2026, 8:44:56 PM
+# Last updated: 3/26/2026, 8:46:46 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
-3#     def __init__(self, x):
-4#         self.val = x
-5#         self.left = None
-6#         self.right = None
-7
-8class Solution(object):
-9    def lowestCommonAncestor(self, root, p, q):
-10        if root is None:
-11            return None
-12        if root.val<p.val and root.val<q.val:
-13            return self.lowestCommonAncestor(root.right,p,q)
-14        if root.val>p.val and root.val>q.val:
-15            return self.lowestCommonAncestor(root.left,p,q)
-16        return root
-17
-18        
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution(object):
+8    def deleteNode(self, root, key):
+9        if root is None:
+10            return None
+11        
+12        if key < root.val:
+13            root.left = self.deleteNode(root.left, key)
+14        
+15        elif key > root.val:
+16            root.right = self.deleteNode(root.right, key)
+17        
+18        else:
+19            if root.left is None:
+20                return root.right
+21            if root.right is None:
+22                return root.left
+23            
+24            temp = root.right
+25            while temp.left:
+26                temp = temp.left
+27            
+28            root.val = temp.val
+29            
+30            root.right = self.deleteNode(root.right, temp.val)
+31        
+32        return root        
