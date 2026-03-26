218083792ef04c3dcb08deace9cd5f4335030ed7
Aryan <jumdearyan7@gmail.com>
2026-03-26 20:44:56 +0530

    File updated at 3/26/2026, 8:44:56 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,21 +1,19 @@
-# Last updated: 3/26/2026, 8:38:52 PM
+# Last updated: 3/26/2026, 8:44:56 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution(object):
-8    def isValidBST(self, root):
-9        
-10        def check(node, low, high):
-11            if node is None:
-12                return True
-13            
-14            if not (low < node.val < high):
-15                return False
-16            
-17            return (check(node.left, low, node.val) and
-18                    check(node.right, node.val, high))
-19        
-20        return check(root, float('-inf'), float('inf'))        
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.left = None
+6#         self.right = None
+7
+8class Solution(object):
+9    def lowestCommonAncestor(self, root, p, q):
+10        if root is None:
+11            return None
+12        if root.val<p.val and root.val<q.val:
+13            return self.lowestCommonAncestor(root.right,p,q)
+14        if root.val>p.val and root.val>q.val:
+15            return self.lowestCommonAncestor(root.left,p,q)
+16        return root
+17
+18        
