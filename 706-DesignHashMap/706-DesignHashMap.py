40d24f002b3116a3a4e400c1733ed2db7703c705
Aryan <jumdearyan7@gmail.com>
2026-03-26 10:54:50 +0530

    File updated at 3/26/2026, 10:54:49 AM

--- 706-DesignHashMap/706-DesignHashMap.py
+++ 706-DesignHashMap/706-DesignHashMap.py
@@ -1,21 +1,12 @@
-# Last updated: 3/26/2026, 10:45:39 AM
-1class MyHashMap:
-2
-3    def __init__(self):
-4        self.map = [-1] * 1000001   
-5
-6    def put(self, key, value):
-7        self.map[key] = value
-8
-9    def get(self, key):
-10        return self.map[key]
-11
-12    def remove(self, key):
-13        self.map[key] = -1
-14
-15
-16# Your MyHashMap object will be instantiated and called as such:
-17# obj = MyHashMap()
-18# obj.put(key,value)
-19# param_2 = obj.get(key)
-20# obj.remove(key)
+# Last updated: 3/26/2026, 10:54:49 AM
+1class Solution(object):
+2    def groupAnagrams(self, strs):
+3        x={}
+4        for word in strs:
+5            key=''.join(sorted(word))
+6            if key not in x:
+7                x[key]=[]
+8            x[key].append(word)
+9        return(list(x.values()))
+10
+11        
