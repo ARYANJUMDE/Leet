950c7539fdcb705efc3fdcab69910bea33c11e81
Aryan <jumdearyan7@gmail.com>
2026-04-01 16:36:23 +0530

    Create 1662-CheckIfTwoStringArraysareEquivalent

---
+++ 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.py
@@ -0,0 +1,10 @@
+# Last updated: 4/1/2026, 4:36:22 PM
+1class Solution(object):
+2    def arrayStringsAreEqual(self, word1, word2):
+3        word1=''.join(word1)
+4        word2=''.join(word2)
+5        if(word1==word2):
+6            return(True)
+7        else:
+8            return(False)
+9        
