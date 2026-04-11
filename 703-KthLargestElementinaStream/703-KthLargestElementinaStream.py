0d8e2f87d1d0c34e75b5623e2ddafdfc5430f278
Aryan <jumdearyan7@gmail.com>
2026-04-11 14:04:55 +0530

    File updated at 4/11/2026, 2:04:55 PM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
@@ -1,26 +1,25 @@
-# Last updated: 4/11/2026, 1:11:34 PM
-1import heapq
-2class KthLargest(object):
-3
-4    def __init__(self, k, nums):
-5        self.k=k
-6        self.heap=nums
-7
-8        heapq.heapify(self.heap)
-9        while len(self.heap)>k:
-10             heapq.heappop(self.heap)
-11
-12    def add(self, val):
-13        heapq.heappush(self.heap, val)
-14        
-15        if len(self.heap) > self.k:
-16            heapq.heappop(self.heap)
-17        
-18        return self.heap[0]    
-19        
-20        
-21
-22
-23# Your KthLargest object will be instantiated and called as such:
-24# obj = KthLargest(k, nums)
-25# param_1 = obj.add(val)
+# Last updated: 4/11/2026, 2:04:55 PM
+1class Solution(object):
+2    def eraseOverlapIntervals(self, intervals):
+3        """
+4        :type intervals: List[List[int]]
+5        :rtype: int
+6        """
+7        if not intervals:
+8            return 0
+9
+10        
+11        intervals.sort(key=lambda x: x[1])
+12
+13        count = 0
+14        prev_end = intervals[0][1]
+15
+16        for i in range(1, len(intervals)):
+17            if intervals[i][0] >= prev_end:
+18                
+19                prev_end = intervals[i][1]
+20            else:
+21            
+22                count += 1
+23
+24        return count
