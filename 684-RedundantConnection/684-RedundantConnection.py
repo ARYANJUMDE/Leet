2c2fa41f09e8074bd284710d469fbcd8581f967c
Aryan <jumdearyan7@gmail.com>
2026-04-13 17:48:00 +0530

    File updated at 4/13/2026, 5:48:00 PM

--- 684-RedundantConnection/684-RedundantConnection.py
+++ 684-RedundantConnection/684-RedundantConnection.py
@@ -1,27 +1,36 @@
-# Last updated: 4/13/2026, 5:45:12 PM
-1class Solution(object):
-2    def findRedundantConnection(self, edges):
-3        parent = {}
-4
-5        def find(x):
-6            if parent[x] != x:
-7                parent[x] = find(parent[x])  # path compression
-8            return parent[x]
-9
-10        def union(x, y):
-11            px = find(x)
-12            py = find(y)
-13            if px == py:
-14                return False  
-15            parent[px] = py
-16            return True
-17
+# Last updated: 4/13/2026, 5:48:00 PM
+1from collections import defaultdict, deque
+2
+3class Solution(object):
+4    def findOrder(self, numCourses, prerequisites):
+5        graph = defaultdict(list)
+6        indegree = [0] * numCourses
+7        
+8        
+9        for a, b in prerequisites:
+10            graph[b].append(a)
+11            indegree[a] += 1
+12        
+13        
+14        q = deque()
+15        for i in range(numCourses):
+16            if indegree[i] == 0:
+17                q.append(i)
 18        
-19        for u, v in edges:
-20            parent[u] = u
-21            parent[v] = v
-22
-23        
-24        for u, v in edges:
-25            if not union(u, v):
-26                return [u, v]    
+19        result = []
+20        
+21        while q:
+22            node = q.popleft()
+23            result.append(node)
+24            
+25            for nei in graph[node]:
+26                indegree[nei] -= 1
+27                if indegree[nei] == 0:
+28                    q.append(nei)
+29        
+30        
+31        if len(result) == numCourses:
+32            return result
+33        else:
+34            return []
+35        
