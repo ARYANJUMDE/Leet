d95d19631403385da9d6af473c748744c8df432a
Aryan <jumdearyan7@gmail.com>
2026-04-13 17:54:47 +0530

    File updated at 4/13/2026, 5:54:46 PM

--- 684-RedundantConnection/684-RedundantConnection.py
+++ 684-RedundantConnection/684-RedundantConnection.py
@@ -1,36 +1,34 @@
-# Last updated: 4/13/2026, 5:48:00 PM
-1from collections import defaultdict, deque
+# Last updated: 4/13/2026, 5:54:46 PM
+1from collections import deque
 2
 3class Solution(object):
-4    def findOrder(self, numCourses, prerequisites):
-5        graph = defaultdict(list)
-6        indegree = [0] * numCourses
-7        
-8        
-9        for a, b in prerequisites:
-10            graph[b].append(a)
-11            indegree[a] += 1
-12        
-13        
-14        q = deque()
-15        for i in range(numCourses):
-16            if indegree[i] == 0:
-17                q.append(i)
+4    def shortestPathBinaryMatrix(self, grid):
+5        n = len(grid)
+6        
+7    
+8        if grid[0][0] != 0 or grid[n-1][n-1] != 0:
+9            return -1
+10        
+11        
+12        directions = [(-1,-1), (-1,0), (-1,1),
+13                      (0,-1),        (0,1),
+14                      (1,-1), (1,0), (1,1)]
+15        
+16        q = deque([(0, 0, 1)])  
+17        grid[0][0] = 1  
 18        
-19        result = []
-20        
-21        while q:
-22            node = q.popleft()
-23            result.append(node)
-24            
-25            for nei in graph[node]:
-26                indegree[nei] -= 1
-27                if indegree[nei] == 0:
-28                    q.append(nei)
-29        
-30        
-31        if len(result) == numCourses:
-32            return result
-33        else:
-34            return []
-35        
+19        while q:
+20            r, c, dist = q.popleft()
+21            
+22        
+23            if r == n-1 and c == n-1:
+24                return dist
+25            
+26            for dr, dc in directions:
+27                nr, nc = r + dr, c + dc
+28                
+29                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
+30                    q.append((nr, nc, dist + 1))
+31                    grid[nr][nc] = 1 
+32        
+33        return -1
