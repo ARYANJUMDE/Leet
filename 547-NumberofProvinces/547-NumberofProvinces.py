2372c4a5e90b4ad3c009a4b99d3101ebd3fe70b9
Aryan <jumdearyan7@gmail.com>
2026-04-13 09:06:55 +0530

    File updated at 4/13/2026, 9:06:56 AM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,21 +1,28 @@
-# Last updated: 4/13/2026, 9:04:17 AM
-1class Solution(object):
-2    def findCircleNum(self, isConnected):
-3        n = len(isConnected)
-4        visited = [False] * n
-5        
-6        def dfs(city):
-7            for neighbor in range(n):
-8                if isConnected[city][neighbor] == 1 and not visited[neighbor]:
-9                    visited[neighbor] = True
-10                    dfs(neighbor)
-11        
-12        provinces = 0
+# Last updated: 4/13/2026, 9:06:56 AM
+1import heapq
+2from collections import Counter
+3class Solution(object):
+4    def reorganizeString(self, s):
+5        freq = Counter(s)
+6        
+7        maxHeap = []
+8        for ch in freq:
+9            heapq.heappush(maxHeap, (-freq[ch], ch))
+10        
+11        prev = (0, '')
+12        result = ""
 13        
-14        for i in range(n):
-15            if not visited[i]:
-16                visited[i] = True
-17                dfs(i)
-18                provinces += 1
-19        
-20        return provinces        
+14        while maxHeap:
+15            count, ch = heapq.heappop(maxHeap)
+16            result += ch
+17            
+18            if prev[0] < 0:
+19                heapq.heappush(maxHeap, prev)
+20            
+21            count += 1  
+22            prev = (count, ch)
+23        
+24        if len(result) != len(s):
+25            return ""
+26        
+27        return result        
