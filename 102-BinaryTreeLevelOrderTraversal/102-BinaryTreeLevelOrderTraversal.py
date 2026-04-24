e3e8bf75df3e38bf52c3d165f31b3451d85755f6
Aryan <jumdearyan7@gmail.com>
2026-04-24 12:09:52 +0530

    File updated at 4/24/2026, 12:09:52 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 3/26/2026, 8:53:38 PM
+# Last updated: 4/24/2026, 12:09:52 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,16 +6,24 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution(object):
-8    def maxPathSum(self, root):
-9        self.max_sum=float('-inf')
-10        def dfs(node):
-11            if not node:
-12                return 0
-13            left=max(dfs(node.left),0)
-14            right=max(dfs(node.right),0)
-15            self.max_sum=max(self.max_sum,left+right+node.val)
-16            return node.val+max(left,right)
-17
-18        dfs(root)
-19        return self.max_sum
-20        
+8    def levelOrder(self, root):
+9        if root is None:
+10            return []
+11        queue=deque()
+12        result=[]
+13        
+14        queue.append(root)
+15        while queue:
+16            level_size=len(queue)
+17            level=[]
+18            for i in range(level_size):
+19                node=queue.popleft()
+20                level.append(node.val)
+21                if node.left:
+22                    queue.append(node.left)
+23                if node.right:
+24                    queue.append(node.right)
+25            result.append(level)
+26        return result
+27    
+28        
