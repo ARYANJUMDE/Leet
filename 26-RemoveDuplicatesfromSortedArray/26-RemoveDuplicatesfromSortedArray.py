a035205a96f8ac9718e9e442156977ea1e752cf9
Aryan <jumdearyan7@gmail.com>
2026-06-27 22:33:28 +0530

    File updated at 6/27/2026, 10:33:28 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,15 +1,20 @@
-# Last updated: 6/27/2026, 8:19:33 PM
+# Last updated: 6/27/2026, 10:33:28 PM
 1class Solution(object):
-2    def runningSum(self, nums):
-3        x=[]
-4        for i in range(len(nums)):
-5            x.append(sum(nums[:i+1]))
-6        return(x)
-7        # runningsum=[]
-8        # t=0
-9        # for i in range(len(nums)):
-10        #     t=t+nums[i]
-11        #     runningsum.append(t)
-12        
-13        # return(runningsum)
-14        
+2    def pivotIndex(self, nums):
+3        # x=-1
+4        # for i in range(0,len(nums)):
+5        #     if(sum(nums[0:i])==sum(nums[i+1:len(nums)+1])):
+6        #         x=i
+7        #         break
+8        # if(x!=-1):
+9        #     return(x)
+10        # else:
+11        #     return(-1)
+12        for i in range(len(nums)):
+13            if sum(nums[:i])==sum(nums[i+1:]):
+14                return(i)
+15        else:
+16            return(-1)
+17    
+18
+19        
