5665b14f350fb1791974eeb6c8ba442fc07554f9
Aryan <jumdearyan7@gmail.com>
2026-06-27 18:00:52 +0530

    File updated at 6/27/2026, 6:00:51 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,29 +1,19 @@
-# Last updated: 6/27/2026, 5:56:51 PM
+# Last updated: 6/27/2026, 6:00:51 PM
 1class Solution(object):
-2    def removeDuplicates(self, nums):
-3        # i=0
-4        # while i<len(nums):
-5        #     j=i+1
-6        #     while j<len(nums):
-7        #         if nums[i]==nums[j]:
-8        #             nums.pop(j)
-9        #         else:
-10        #             j=j+1
-11        #     i=i+1
-12        # return len(nums)
-13        
-14        x=list(set(nums))
-15        for i in range(len(x)):
-16            if nums.count(x[i])>1:
-17                while nums.count(x[i])>1:
-18                    nums.remove(x[i])
-19        return(len(nums))   
-20
-21
-22
-23
-24        
-25
-26
-27
-28
+2    def removeElement(self, nums, val):
+3        # i=len(nums)-1
+4        # while(i>=0):
+5        #     if(nums[i]==val):
+6        #         nums.pop(i)
+7        #     i=i-1
+8        # return(len(nums))
+9        # return(nums)
+10        x=list(set(nums))
+11        for i in range(len(x)):
+12            if x[i]==val:
+13                while nums.count(x[i])>0:
+14                    nums.remove(x[i])
+15        return(len(nums))
+16
+17
+18        
