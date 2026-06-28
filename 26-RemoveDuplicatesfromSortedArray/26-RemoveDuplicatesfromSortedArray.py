23202bd7aface83650524e7b512267633664ac99
Aryan <jumdearyan7@gmail.com>
2026-06-28 12:52:17 +0530

    File updated at 6/28/2026, 12:52:17 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,20 +1,14 @@
-# Last updated: 6/27/2026, 10:33:28 PM
+# Last updated: 6/28/2026, 12:52:17 PM
 1class Solution(object):
-2    def pivotIndex(self, nums):
-3        # x=-1
-4        # for i in range(0,len(nums)):
-5        #     if(sum(nums[0:i])==sum(nums[i+1:len(nums)+1])):
-6        #         x=i
-7        #         break
-8        # if(x!=-1):
-9        #     return(x)
-10        # else:
-11        #     return(-1)
-12        for i in range(len(nums)):
-13            if sum(nums[:i])==sum(nums[i+1:]):
-14                return(i)
-15        else:
-16            return(-1)
-17    
-18
-19        
+2    def sortedSquares(self, nums):
+3        # x=[]
+4        # for num in nums:
+5        #     x.append(num*num)
+6        # x.sort()
+7        # return x
+8        for i in range(len(nums)):
+9            nums[i]=nums[i]**2
+10        nums.sort()
+11        return(nums)
+12
+13        
