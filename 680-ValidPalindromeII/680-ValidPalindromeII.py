7e79298bea1f743ce36a622f1dc0ecbe64259c10
Aryan <jumdearyan7@gmail.com>
2026-07-01 14:47:48 +0530

    File updated at 7/1/2026, 2:47:47 PM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,22 +1,10 @@
-# Last updated: 6/30/2026, 9:27:53 PM
+# Last updated: 7/1/2026, 2:47:47 PM
 1class Solution(object):
-2    def mergeAlternately(self, word1, word2):
-3        t=""
-4        v=""
-5        z=abs(len(word1)-len(word2))
-6        if z!=0:
-7            if min(len(word1),len(word2))==len(word1):
-8                for i in range(z):
-9                    word1=word1+" "
-10            else:
-11                for i in range(z):
-12                    word2=word2+" "
-13        for i in range(len(word1)):
-14            t=t+word1[i]
-15            t=t+word2[i]
-16        for i in range(len(t)):
-17            if t[i]!=" ":
-18                v=v+t[i]
-19        return(v)
-20
-21        
+2    def countSubstrings(self, s):
+3        x=[]
+4        for i in range(len(s)):
+5            for j in range(i+1,len(s)+1):
+6                if s[i:j]==s[i:j][::-1]:
+7                    x.append(s[i:j])
+8        return(len(x))
+9        
