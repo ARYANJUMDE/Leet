3ddc6eb27ce9f3875f21ab5f9a42964bcf87c6e0
Aryan <jumdearyan7@gmail.com>
2026-07-13 22:12:58 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.py
@@ -0,0 +1,18 @@
+# Last updated: 7/13/2026, 10:12:56 PM
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        result=[]
+        def backtrack(start, path, total):
+            if total == target:
+                result.append(path[:])
+                return
+            if total > target:
+                return
+            for i in range(start, len(candidates)):
+                
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])
+                path.pop()
+        backtrack(0, [], 0)
+        return result
+        
