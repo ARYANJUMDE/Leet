ceb81e6ecf6e52ee8e7138bb7d0a7763ab6e3ca0
Aryan <jumdearyan7@gmail.com>
2026-07-13 22:13:07 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,20 @@
+# Last updated: 7/13/2026, 10:13:05 PM
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        for ch in s:
+            if(ch in "({["):
+                stack.append(ch)
+            else:
+                if len(stack)==0:
+                    return False
+                top=stack.pop()
+
+                if ((ch==")" and top!="(") or (ch=="]" and top!="[") or (ch=="}" and top!="{")):
+
+
+                    return False
+        return len(stack)==0
+
+        
+        
