31b6882bf170a4c12f20e30138b1e4476bc1b24a
Aryan <jumdearyan7@gmail.com>
2026-08-13 18:03:25 +0530

    File updated at 8/13/2026, 6:03:25 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,10 +1,12 @@
-# Last updated: 12/27/2025, 6:58:07 PM
-class Solution(object):
-    def climbStairs(self, n):
-        a=0
-        b=1
-        for i in range(0,n):
-            a,b=b,a+b
-            
-        return (b)
-        
+# Last updated: 8/13/2026, 6:03:25 PM
+1class Solution(object):
+2    def climbStairs(self, n):
+3        if n==1 or n==0:
+4            return 1
+5        dp=[-1]*(n+1)
+6        dp[0]=1
+7        dp[1]=1
+8        for i in range(2,len(dp)):
+9            dp[i]=dp[i-1]+dp[i-2]
+10        return dp[n]
+11        
