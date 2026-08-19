913641e12dafe0d474cc5ed60b43dd6c6c192b0d
Aryan <jumdearyan7@gmail.com>
2026-08-19 16:21:00 +0530

    Create 491-NonDecreasingSubsequences

---
+++ 491-NonDecreasingSubsequences/491-NonDecreasingSubsequences.py
@@ -0,0 +1,16 @@
+# Last updated: 8/19/2026, 4:21:00 PM
+class Solution(object):
+    def findSubsequences(self, nums):
+        result=[]
+        def solve(a,i):
+            if i==len(nums):
+                if len(a)>=2 and a==sorted(a,reverse=False) and a not in result  :
+                    result.append(a[:])
+                    return
+            else:
+                a.append(nums[i])
+                solve(a,i+1)
+                a.pop()
+                solve(a,i+1)
+        solve([],0)
+        return result
