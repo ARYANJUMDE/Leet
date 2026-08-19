5ab7e94601d1322acf299d42a83eddf4818cc682
Aryan <jumdearyan7@gmail.com>
2026-08-19 16:18:41 +0530

    Create 1044-FindCommonCharacters

---
+++ 1044-FindCommonCharacters/1044-FindCommonCharacters.py
@@ -0,0 +1,18 @@
+# Last updated: 8/19/2026, 4:18:40 PM
+class Solution(object):
+    def commonChars(self, words):
+        t=words[0]
+        z=""
+        x=[]
+        for ch in t:
+            if ch not in z:
+                z=z+ch
+        for i in range(len(z)):
+            p=[]
+            for j in range(len(words)):
+                p.append(words[j].count(z[i]))
+            if min(p)>0:
+                x.extend((z[i]*min(p)))
+        return(x)
+
+        
