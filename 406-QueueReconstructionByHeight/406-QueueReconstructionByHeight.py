97e9fae6c698f408cee5c22a49d3061ef9611f0b
Aryan <jumdearyan7@gmail.com>
2026-08-19 16:21:20 +0530

    Create 406-QueueReconstructionByHeight

---
+++ 406-QueueReconstructionByHeight/406-QueueReconstructionByHeight.py
@@ -0,0 +1,11 @@
+# Last updated: 8/19/2026, 4:21:19 PM
+class Solution(object):
+    def reconstructQueue(self, people):
+        people=sorted(people,key=lambda x:(-x[0],x[1]))
+        x=[]
+        for i in range (len(people)):
+            x.insert(people[i][1],people[i])
+        
+        
+        return(x)
+        
