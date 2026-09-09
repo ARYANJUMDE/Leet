e52c6c702e8c9ae22fb4ec127ab993c32c948e97
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:13:44 +0530

    Create 1394-FindLuckyIntegerInAnArray

---
+++ 1394-FindLuckyIntegerInAnArray/1394-FindLuckyIntegerInAnArray.py
@@ -0,0 +1,18 @@
+# Last updated: 9/9/2026, 10:13:43 PM
+class Solution(object):
+    def findLucky(self, arr):
+        x=[]
+        y=[]
+        for num in arr:
+            if num not in x:
+                x.append(num)
+        for num in x:
+            if num==arr.count(num):
+                y.append(num)
+        if(len(y)==0):
+            return(-1)
+        
+        else:
+            return(max(y))
+
+        
