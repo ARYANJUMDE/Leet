77b7d7fd4538e550282b46af68ab43a347280efe
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:14:41 +0530

    Create 1207-UniqueNumberOfOccurrences

---
+++ 1207-UniqueNumberOfOccurrences/1207-UniqueNumberOfOccurrences.py
@@ -0,0 +1,12 @@
+# Last updated: 9/9/2026, 10:14:40 PM
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        x=list(set(arr))
+        y=[]
+        for i in range(len(x)):
+            y.append(arr.count(x[i]))
+        if len(set(y))==len(y):
+            return(True)
+        else:
+            return(False)
+        
