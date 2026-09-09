1dd8a09dc64e36bbf6f2dc314b26ed2cc9a4964a
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:10:41 +0530

    Create 2418-SortThePeople

---
+++ 2418-SortThePeople/2418-SortThePeople.py
@@ -0,0 +1,16 @@
+# Last updated: 9/9/2026, 10:10:40 PM
+class Solution(object):
+    def sortPeople(self, names, heights):
+        x=[]
+        for i in range (len(names)):
+            x.append([names[i],heights[i]])
+        x.sort(key=lambda t:t[1],reverse=True)
+        y=[]
+        for i in range(len(x)):
+            y.append(x[i][0])
+        
+        
+        return(y)
+
+
+        
