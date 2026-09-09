437276875eb2c20207bc5ecbbf5b8e947cc3cf60
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:10:11 +0530

    Create 2535-DifferenceBetweenElementSumAndDigitSumOfAnArray

---
+++ 2535-DifferenceBetweenElementSumAndDigitSumOfAnArray/2535-DifferenceBetweenElementSumAndDigitSumOfAnArray.py
@@ -0,0 +1,14 @@
+# Last updated: 9/9/2026, 10:10:10 PM
+class Solution(object):
+    def differenceOfSum(self, nums):
+        t=sum(nums)
+        y=0
+        for i in range(len(nums)):
+            while nums[i]>0:
+                x=nums[i]%10
+                y=y+x
+                nums[i]=nums[i]//10
+        
+        
+        return(abs(t-y))
+        
