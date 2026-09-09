52070b5a34fd94ef67f89bf48413c1f1561775d5
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:10:43 +0530

    Create 2413-SmallestEvenMultiple

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.py
@@ -0,0 +1,9 @@
+# Last updated: 9/9/2026, 10:10:42 PM
+import math
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        if n%2==0:
+            return n
+        else:
+            return n*2
+
