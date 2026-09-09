cb555a621472aa89ba5db735d90d6136fc6742bd
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:10:37 +0530

    Create 2427-NumberOfCommonFactors

---
+++ 2427-NumberOfCommonFactors/2427-NumberOfCommonFactors.py
@@ -0,0 +1,10 @@
+# Last updated: 9/9/2026, 10:10:36 PM
+class Solution(object):
+    def commonFactors(self, a, b):
+        z=min(a,b)
+        count=0
+        for i in range(1,z+1):
+            if a%i==0 and b%i==0:
+                count=count+1
+        return(count)
+        
