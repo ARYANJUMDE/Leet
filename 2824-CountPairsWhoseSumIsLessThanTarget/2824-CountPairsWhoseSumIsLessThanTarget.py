6e0c9def272b94069d455454e32391b841059137
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:09:49 +0530

    Create 2824-CountPairsWhoseSumIsLessThanTarget

---
+++ 2824-CountPairsWhoseSumIsLessThanTarget/2824-CountPairsWhoseSumIsLessThanTarget.py
@@ -0,0 +1,11 @@
+# Last updated: 9/9/2026, 10:09:48 PM
+class Solution(object):
+    def countPairs(self, nums, target):
+        count=0
+        for i in range(0,len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]<target:
+                    count=count+1
+        return(count)
+
+        
