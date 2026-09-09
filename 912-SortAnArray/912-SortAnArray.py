49ff506704c35e049a04dc5ddc8e132982d22227
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:16:00 +0530

    Create 912-SortAnArray

---
+++ 912-SortAnArray/912-SortAnArray.py
@@ -0,0 +1,14 @@
+# Last updated: 9/9/2026, 10:16:00 PM
+class Solution(object):
+    def sortArray(self, nums):
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if(nums[j]<nums[i]):
+        #             nums[i],nums[j]=nums[j],nums[i]
+        
+        
+        # return(nums)
+        nums.sort()
+        return nums
+        
+        
