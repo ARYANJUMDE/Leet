c758691db6c3500ff54eeae459dcf734d01ce306
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:13:17 +0530

    Create 1480-RunningSumOf1dArray

---
+++ 1480-RunningSumOf1dArray/1480-RunningSumOf1dArray.py
@@ -0,0 +1,15 @@
+# Last updated: 9/9/2026, 10:13:16 PM
+class Solution(object):
+    def runningSum(self, nums):
+        x=[]
+        for i in range(len(nums)):
+            x.append(sum(nums[:i+1]))
+        return(x)
+        # runningsum=[]
+        # t=0
+        # for i in range(len(nums)):
+        #     t=t+nums[i]
+        #     runningsum.append(t)
+        
+        # return(runningsum)
+        
