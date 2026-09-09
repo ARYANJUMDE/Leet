e7f6a4cb43ad31c4214d2fc5785d512255c938fd
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:15:24 +0530

    Create 1042-FlowerPlantingWithNoAdjacent

---
+++ 1042-FlowerPlantingWithNoAdjacent/1042-FlowerPlantingWithNoAdjacent.py
@@ -0,0 +1,18 @@
+# Last updated: 9/9/2026, 10:15:23 PM
+class Solution(object):
+    def gardenNoAdj(self, n, paths):
+        adj = [[] for _ in range(n)]
+        for x, y in paths:
+            adj[x-1].append(y-1)
+            adj[y-1].append(x-1)
+        
+        res = [0] * n
+        for i in range(n):
+            used = {res[j] for j in adj[i] if res[j] != 0}
+            for color in range(1, 5):
+                if color not in used:
+                    
+                    res[i] = color
+                    break
+        return res
+        
