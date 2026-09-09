ade749f46bcf0cb4c2c31e9796c9a32629e9a602
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:10:00 +0530

    Create 2784-CheckIfArrayIsGood

---
+++ 2784-CheckIfArrayIsGood/2784-CheckIfArrayIsGood.py
@@ -0,0 +1,14 @@
+# Last updated: 9/9/2026, 10:09:59 PM
+class Solution(object):
+    def isGood(self, nums):
+        z=max(nums)
+        if(nums.count(z)==2):
+            for i in range(1,z):
+                if i not in nums or nums.count(i)>1:
+                    return(False)
+            else:
+                return(True)
+        else:
+            return(False)
+    
+    
