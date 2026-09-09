945322a75717d39033e074065a49786007389e46
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:13:39 +0530

    Create 1413-MinimumValueToGetPositiveStepByStepSum

---
+++ 1413-MinimumValueToGetPositiveStepByStepSum/1413-MinimumValueToGetPositiveStepByStepSum.py
@@ -0,0 +1,16 @@
+# Last updated: 9/9/2026, 10:13:39 PM
+class Solution(object):
+    def minStartValue(self, nums):
+        for i in range(1,9999):
+            add=i
+            count=0
+            for j in range(len(nums)):
+                add=add+nums[j]
+                if add<1:
+                    break
+                else:
+                    count=count+1
+            if count==len(nums):
+                return(i)
+
+    
