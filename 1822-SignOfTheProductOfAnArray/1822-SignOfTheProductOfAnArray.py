7553c1d3e129fc5bb848025d65b609205aa5235e
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:12:17 +0530

    Create 1822-SignOfTheProductOfAnArray

---
+++ 1822-SignOfTheProductOfAnArray/1822-SignOfTheProductOfAnArray.py
@@ -0,0 +1,15 @@
+# Last updated: 9/9/2026, 10:12:16 PM
+class Solution(object):
+    def arraySign(self, nums):
+        x=1
+        for i in range(len(nums)):
+            x=x*nums[i]
+        return self.signFunc(x)
+    def signFunc(self,x):
+        if x==0:
+            return 0
+        if x>0:
+            return 1
+        return -1
+
+        
