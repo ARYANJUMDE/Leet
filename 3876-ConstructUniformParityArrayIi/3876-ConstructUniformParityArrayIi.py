00534af11888b85b9b942ba185e601193879879b
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:08:27 +0530

    Create 3876-ConstructUniformParityArrayIi

---
+++ 3876-ConstructUniformParityArrayIi/3876-ConstructUniformParityArrayIi.py
@@ -0,0 +1,25 @@
+# Last updated: 9/9/2026, 10:08:25 PM
+class Solution(object):
+    def uniformArray(self, nums1):
+        count=0
+        for i in range(len(nums1)):
+            if nums1[i]%2==0:
+                count=count+1
+        if count==len(nums1):
+            return True
+        if min(nums1)%2==0:
+            return False
+        return True
+
+        # nums1.sort()
+        # count=0
+        # for i in range(len(nums1)):
+        #     if nums1[i]%2!=0:
+        #         count=count+1
+        #     if nums1[i]%2==0 and count>0:
+        #         continue
+        #     else:
+        #         return False
+        # return True
+            
+        
