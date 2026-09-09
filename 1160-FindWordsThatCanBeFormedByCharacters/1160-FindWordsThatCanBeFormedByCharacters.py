13865ce268e401b0f5884236d5664c78b16ce436
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:15:29 +0530

    Create 1160-FindWordsThatCanBeFormedByCharacters

---
+++ 1160-FindWordsThatCanBeFormedByCharacters/1160-FindWordsThatCanBeFormedByCharacters.py
@@ -0,0 +1,12 @@
+# Last updated: 9/9/2026, 10:15:28 PM
+class Solution(object):
+    def countCharacters(self, words, chars):
+        x=0
+        for i in range(len(words)):
+            if all(words[i].count(ch) <= chars.count(ch) for ch in words[i]):
+                x=x+len(words[i])
+        
+        
+        return(x)
+
+        
