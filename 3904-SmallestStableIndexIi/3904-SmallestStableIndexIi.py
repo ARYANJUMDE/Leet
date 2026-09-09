ea3dac08d7883ebdb59b372562e0ce5604e5e124
Aryan <jumdearyan7@gmail.com>
2026-09-09 22:08:21 +0530

    Create 3904-SmallestStableIndexIi

---
+++ 3904-SmallestStableIndexIi/3904-SmallestStableIndexIi.py
@@ -0,0 +1,15 @@
+# Last updated: 9/9/2026, 10:08:20 PM
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        min1=[0]*len(nums)
+        min1[-1]=nums[-1]
+        for i in range(len(nums)-2,-1,-1):
+            min1[i]=min(nums[i],min1[i+1])
+        max1=nums[0]
+        for i in range(len(nums)):
+            if max1<nums[i]:
+                max1=nums[i]
+            if max1-min1[i]<=k:
+                return(i)
+        return -1
+        
