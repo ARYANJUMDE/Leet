6a37c7c176db3af0edb1a5c4bf889d6340dfd88d
Aryan <jumdearyan7@gmail.com>
2026-09-14 14:15:33 +0530

    File updated at 9/14/2026, 2:15:32 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,60 +1,32 @@
-# Last updated: 9/13/2026, 1:37:43 PM
+# Last updated: 9/14/2026, 2:15:32 PM
 1class Solution(object):
-2    def longestOnes(self, nums, k):
-3        # x=[]
-4        # for i in range(len(nums)):
-5        #     for j in range(i+1,len(nums)+1):
-6        #         if nums[i:j].count(0)<=k:
-7        #             x.append(len(nums[i:j]))
-8        # if len(x)==0:
-9        #     return 0
-10        # return(max(x))
-11        # l=0
-12        # r=0
-13        # max_len=0
-14        # final_len=0
-15        # while r<len(nums):
-16        #     if k>0:
-17        #         max_len=max_len+1
-18        #         if nums[r]==0:
-19        #             k=k-1
-20        #     else:
-21        #         if nums[r]==1:
-22        #             max_len=max_len+1
-23        #         else:
-24        #             while k==0:
-25        #                 if nums[l]==0:
-26        #                     k=k+1
-27        #                 l=l+1
-28        #                 max_len=max_len-1
-29        #             max_len=max_len+1
-30        #             k=k-1
-31        #     if max_len>final_len:
-32        #         final_len=max_len
-33        #     r=r+1
-34        # return final_len
-35        r=0
-36        l=0
-37        curr_len=0
-38        max_len=0
-39        while r<len(nums):
-40            if k>0:
-41                curr_len=curr_len+1
-42                if nums[r]==0:
-43                    k=k-1
-44            elif k==0 and nums[r]!=0:
-45                curr_len=curr_len+1
-46            elif k==0 and nums[r]==0:
-47                curr_len=curr_len+1
-48                while k==0:
-49                    if nums[l]==0:
-50                        k=k+1
-51                    curr_len=curr_len-1
-52                    l=l+1
-53                k=k-1
-54            if curr_len>max_len:
-55                max_len=curr_len
-56            r=r+1
-57        return max_len
-58
-59                    
+2    def numSubarraysWithSum(self, nums, goal):
+3        count1=0
+4        sum1=0
+5        r=0
+6        l=0
+7        while r<len(nums):
+8            sum1=sum1+nums[r]
+9            if sum1>goal:
+10                while sum1>goal:
+11                    sum1=sum1-nums[l]
+12                    l=l+1
+13            count1=count1+(r-l+1)
+14            r=r+1
+15        sum1=0
+16        count2=0
+17        r=0
+18        l=0
+19        if goal==0:
+20            return count1
+21        while r<len(nums):
+22            sum1=sum1+nums[r]
+23            if sum1>goal-1:
+24                while sum1>goal-1:
+25                    sum1=sum1-nums[l]
+26                    l=l+1
+27            count2=count2+(r-l+1)
+28            r=r+1
+29        return abs(count2-count1)
+30        
+31        
