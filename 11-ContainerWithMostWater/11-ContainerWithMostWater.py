ec41165b790084ca0245220db230bd6963ce41e4
Aryan <jumdearyan7@gmail.com>
2026-09-20 18:51:12 +0530

    File updated at 9/20/2026, 6:51:10 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,23 +1,40 @@
-# Last updated: 12/27/2025, 6:58:27 PM
-class Solution(object):
-    def maxArea(self, height):
-        # max=0
-        # for i in range(0,len(height)):
-        #     for j in range(0,len(height)):
-        #         area=min(height[i],height[j])*(j-i)
-        #         if area>max:
-        #             max=area
-        # return max
-        left, right = 0, len(height) - 1
-        max_area = 0
-        while left < right:
-            width = right - left
-            area = min(height[left], height[right]) * width
-            max_area = max(max_area, area)
-            
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-        return max_area
-
+# Last updated: 9/20/2026, 6:51:10 PM
+1class Solution(object):
+2    def maxArea(self, height):
+3        # max=0
+4        # for i in range(0,len(height)):
+5        #     for j in range(0,len(height)):
+6        #         area=min(height[i],height[j])*(j-i)
+7        #         if area>max:
+8        #             max=area
+9        # return max
+10        # left, right = 0, len(height) - 1
+11        # max_area = 0
+12        # while left < right:
+13        #     width = right - left
+14        #     area = min(height[left], height[right]) * width
+15        #     max_area = max(max_area, area)
+16            
+17        #     if height[left] < height[right]:
+18        #         left += 1
+19        #     else:
+20        #         right -= 1
+21        # return max_area
+22
+23        i=0
+24        j=len(height)-1
+25        max_area=0
+26        while i<j:
+27            width=j-i
+28            heights=min(height[i],height[j])
+29            area=width*heights
+30            if area>max_area:
+31                max_area=area
+32            if height[j]>height[i]:
+33                i=i+1
+34            else:
+35                j=j-1
+36        return max_area
+37            
+38
+39
