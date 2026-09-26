8cc94180be310cbf102e465e6833f1d886b90e82
Aryan <jumdearyan7@gmail.com>
2026-09-26 16:14:43 +0530

    Create 3550-SmallestIndexWithDigitSumEqualToIndex

---
+++ 3550-SmallestIndexWithDigitSumEqualToIndex/3550-SmallestIndexWithDigitSumEqualToIndex.py
@@ -0,0 +1,11 @@
+# Last updated: 9/26/2026, 4:14:43 PM
+class Solution(object):
+    def smallestIndex(self, nums):
+        for i in range(len(nums)):
+            sum1=0
+            while nums[i]>0:
+                sum1=sum1+nums[i]%10
+                nums[i]=nums[i]//10
+            if sum1==i:
+                return i
+        return -1
