3e5990dd5401741bc0bdb8977dc5924108da96dd
Aryan <jumdearyan7@gmail.com>
2026-09-26 18:56:40 +0530

    File updated at 9/26/2026, 6:56:40 PM

--- 1798-MaximumNumberofConsecutiveValuesYouCanMake/1798-MaximumNumberofConsecutiveValuesYouCanMake.py
+++ 1798-MaximumNumberofConsecutiveValuesYouCanMake/1798-MaximumNumberofConsecutiveValuesYouCanMake.py
@@ -1,31 +1,16 @@
-# Last updated: 9/26/2026, 6:54:45 PM
-1class Solution(object):
-2    def getMaximumConsecutive(self, coins):
-3        # reach=0
-4        # coins.sort()
-5        # for i in range(len(coins)):
-6        #     if coins[i]>reach+1:
-7        #         break
-8        #     reach=reach+coins[i]
-9        # return reach+1
-10        coins.sort()
-11        # a=[0]
-12        # s=set()
-13        # for i in range(len(coins)):
-14        #     for j in range(len(a)):
-15        #         t=a[j]+coins[i]
-16        #         if t not in s:
-17        #             a.append(t)
-18        #             s.add(t)
-19        #         if a[-2]!=a[-1]-1:
-20        #             a.pop()
-21        #             break
-22        # return(len(a))
-23        coins.sort()
-24        max_val=0
-25        for i in range(len(coins)):
-26            if max_val+1<coins[i]:
-27                break
-28            max_val=max_val+coins[i]
-29        return max_val+1
-30                    
+# Last updated: 9/26/2026, 6:56:40 PM
+class Solution(object):
+    def getMaximumConsecutive(self, coins):
+        m = len(coins)
+        coins.sort()
+        maxreach=0
+        i=0
+        count=1
+        for coin in coins:
+            if i<m and coin<=maxreach+1:
+                maxreach+=coin
+                count+=1
+            else:
+                break
+        
+        return maxreach+1
